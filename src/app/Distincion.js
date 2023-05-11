e0d57824d092bc7f60e1e4e09ac0ad3247d99f67
aAnfitrion <tendondevastador@hotmail.com>
2023-05-11 12:05:31 -0500

    Añadiendo la distincion de donador

--- src/app/Distincion.js
+++ src/app/Distincion.js
@@ -8,12 +8,13 @@ export default function Distincion(props) {
         ${props.tipo == 2 ? 'text-red-400 border-red-400' : ''}
         ${props.tipo == 3 ? 'text-lime-400 border-lime-400' : ''}
         ${props.tipo == 4 ? 'text-sky-300 border-sky-300' : ''}
-        ${props.tipo == 5 ? 'text-amber-400 border-amber-400' : ''}
-        ${props.tipo == 6 ? 'text-orange-400 border-orange-400' : ''}
-        ${props.tipo == 7 ? 'text-pink-400 border-pink-400' : ''}
-        ${props.tipo == 8 ? 'text-rose-300 border-rose-300' : ''}
-        ${props.tipo == 9 ? 'text-violet-400 border-violet-400' : ''}
-        ${props.tipo == 10 ? 'text-orange-200 border-orange-200' : ''}
+        ${props.tipo == 5 ? 'text-fuchsia-300 border-fuchsia-300' : ''}
+        ${props.tipo == 6 ? 'text-amber-400 border-amber-400' : ''}
+        ${props.tipo == 7 ? 'text-orange-400 border-orange-400' : ''}
+        ${props.tipo == 8 ? 'text-pink-400 border-pink-400' : ''}
+        ${props.tipo == 9 ? 'text-rose-300 border-rose-300' : ''}
+        ${props.tipo == 10 ? 'text-violet-400 border-violet-400' : ''}
+        ${props.tipo == 11 ? 'text-orange-200 border-orange-200' : ''}
         
         ${props.tipo == 'T2' ? 'text-green-400 border-green-400' : ''}
         `}
@@ -22,12 +23,13 @@ export default function Distincion(props) {
         {props.tipo == 2 ? 'Mano derecha' : null}
         {props.tipo == 3 ? 'Jefe moderador' : null}
         {props.tipo == 4 ? 'Moderador' : null}
-        {props.tipo == 5 ? 'Programador' : null}
-        {props.tipo == 6 ? 'Publicista' : null}
-        {props.tipo == 7 ? 'Anfitrión' : null}
-        {props.tipo == 8 ? 'Diseñador' : null}
-        {props.tipo == 9 ? 'Soporte' : null}
-        {props.tipo == 10 ? 'Pride guardián' : null}
+        {props.tipo == 5 ? 'Donador' : null}
+        {props.tipo == 6 ? 'Programador' : null}
+        {props.tipo == 7 ? 'Publicista' : null}
+        {props.tipo == 8 ? 'Anfitrión' : null}
+        {props.tipo == 9 ? 'Diseñador' : null}
+        {props.tipo == 10 ? 'Soporte' : null}
+        {props.tipo == 11 ? 'Pride guardián' : null}
 
         {props.tipo == 'T2' ? 'Estelar Tecla 2' : null}
       </span>
@@ -39,12 +41,13 @@ export default function Distincion(props) {
         {props.tipo == 2 ? 'Mano derecha' : null}
         {props.tipo == 3 ? 'Jefe moderador' : null}
         {props.tipo == 4 ? 'Moderador' : null}
-        {props.tipo == 5 ? 'Programador' : null}
-        {props.tipo == 6 ? 'Publicista' : null}
-        {props.tipo == 7 ? 'Anfitrión' : null}
-        {props.tipo == 8 ? 'Diseñador' : null}
-        {props.tipo == 9 ? 'Soporte' : null}
-        {props.tipo == 10 ? 'Pride guardián' : null}
+        {props.tipo == 5 ? 'Donador' : null}
+        {props.tipo == 6 ? 'Programador' : null}
+        {props.tipo == 7 ? 'Publicista' : null}
+        {props.tipo == 8 ? 'Anfitrión' : null}
+        {props.tipo == 9 ? 'Diseñador' : null}
+        {props.tipo == 10 ? 'Soporte' : null}
+        {props.tipo == 11 ? 'Pride guardián' : null}
 
         {props.tipo == 'c' ? 'Común' : null}
         {props.tipo == 'r' ? 'Raro' : null}
