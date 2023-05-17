49c3cdeda2cb169e938b5a36fb1c26b97f965840
aAnfitrion <tendondevastador@hotmail.com>
2023-05-17 01:52:44 -0500

    Mejoramos el aspecto visual

--- src/app/NavBar.js
+++ src/app/NavBar.js
@@ -6,11 +6,11 @@ export default function NavBar() {
     <>
       <nav className='sticky top-0 bg-noche-900/90 border-b border-noche-700/30 backdrop-blur text-stone-300 p-4 flex items-center justify-between'>
         <div className='relative flex items-center'>
-          <Link href='/' className='relative flex items-center pr-2'>
+          <Link href='/' className='relative flex items-center'>
             <Image className='' src='https://media.discordapp.net/attachments/1032516925617614878/1107528348118294548/prelogo1.png?width=458&height=458' height={40} width={40} quality={100} alt='Logo de insomnia' />
-            <h1 className='text-xl font-medium'>Insomnia</h1>
+            <h1 className='text-2xl font-semibold pl-2 pr-4'>Insomnia</h1>
           </Link>
-          <ul className="relative flex items-center gap-5 text-sm">
+          <ul className="relative font-semibold flex items-center gap-5 text-sm">
             <li><Link className='hover:text-white duration-200' href='/destacados'>Destacados</Link></li>
             <li><Link className='hover:text-white duration-200' href='/guias'>Guías</Link></li>
             <li><Link className='hover:text-white duration-200' href='/blog'>Blog</Link></li>
