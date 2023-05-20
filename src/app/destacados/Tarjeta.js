1c29a479bb86dc4afed6cf19d704e66064b4fa15
aAnfitrion <tendondevastador@hotmail.com>
2023-05-19 23:47:37 -0500

    Corrigiendo errores de width

--- src/app/destacados/Tarjeta.js
+++ src/app/destacados/Tarjeta.js
@@ -4,15 +4,15 @@ import Image from 'next/image'
 export default function Tarjeta(props) {
   return (
     <li className="p-2" key={props.key}>
-      <div className="bg-noche-700/20 rounded-lg p-8  flex justify-between items-center flex-wrap">
-        <div>
+      <div className="bg-noche-700/20 rounded-lg p-8 flex justify-between items-center flex-wrap">
+        <div className='w-5/6'>
           <div className='flex items-center'>
             <p className="text-white text-xl pr-2">{ props.usuario }</p>
             <Distincion className='text-sm' tipo={props.tipo} op={props.op} />
           </div>
           <p className='text-noche-300'>{ props.cargo }</p>
         </div>
-        <Image className='rounded-full' src={ props.img_src } height={70} width={70} quality={100} alt={`Foto de perfil de ${props.usuario}`} />
+        <Image className='rounded-full w-1/6' src={ props.img_src } height={70} width={70} quality={100} alt={`Foto de perfil de ${props.usuario}`} />
         <div className='pt-2'>
           <p className="text-noche-100">
             { props.desc }
