d156acd149c37dc3b83ed34923328c6c424be8b1
aAnfitrion <tendondevastador@hotmail.com>
2023-05-19 22:46:38 -0500

    Mejoramos totalmente el diseño de las tarjetas de perfiles

--- src/app/destacados/page.js
+++ src/app/destacados/page.js
@@ -1,28 +1,67 @@
-import NavBar from '../NavBar'
 import Distincion from '../Distincion'
 import Tarjeta from './Tarjeta';
 import { perfiles } from '../Perfiles'
+import Image from 'next/image';
+
+const col1 = perfiles.filter(perfil => perfil.columna === 1)
+const col2 = perfiles.filter(perfil => perfil.columna === 2)
+const col3 = perfiles.filter(perfil => perfil.columna === 3)
 
 export default function Destacados() {
   return (
     <>
-			{perfiles.map(perfil => (
-				<Tarjeta
-					key={perfil.usuario}
-					bg={perfil.bg}
-					src={perfil.img_src}
-					usuario={perfil.usuario}
-					rareza={perfil.rareza}
-					desc={perfil.desc}
-					discord={perfil.discord}
-					nacion={perfil.nacion}
-					edad={perfil.edad}
-				>
-					{perfil.distinciones.map(distinciones => (
-						<Distincion key={perfil.usuario} tipo={distinciones.distincion} op={perfil.op} />
-					))}
-				</Tarjeta>
-			))}
+			<div className="my-4 xl:mx-16 mx-4">
+				<div className="grid grid-cols-12">
+					<div className="lg:col-span-4 col-span-12">
+						<ul className='list-none'>
+							{col1.map(perfil => {
+								return (
+									<Tarjeta
+										key={perfil.usuario}
+										usuario={perfil.usuario}
+										tipo={perfil.distincion}
+										cargo={perfil.cargo}
+										img_src={perfil.img_src}
+										desc={perfil.desc}
+									/>
+								)
+							})}
+						</ul>
+					</div>		
+					<div className="lg:col-span-4 col-span-12">
+						<ul className='list-none'>
+							{col2.map(perfil => {
+								return (
+									<Tarjeta
+										key={perfil.usuario}
+										usuario={perfil.usuario}
+										tipo={perfil.distincion}
+										cargo={perfil.cargo}
+										img_src={perfil.img_src}
+										desc={perfil.desc}
+									/>
+								)
+							})}
+						</ul>
+					</div>		
+					<div className="lg:col-span-4 col-span-12">
+						<ul className='list-none'>
+							{col3.map(perfil => {
+								return (
+									<Tarjeta
+										key={perfil.usuario}
+										usuario={perfil.usuario}
+										tipo={perfil.distincion}
+										cargo={perfil.cargo}
+										img_src={perfil.img_src}
+										desc={perfil.desc}
+									/>
+								)
+							})}
+						</ul>
+					</div>		
+				</div>
+			</div>
     </>
   )
 }
