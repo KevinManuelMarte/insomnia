9a411203fbebd488078954def2e0f42a96f62667
aAnfitrion <tendondevastador@hotmail.com>
2023-05-20 01:31:22 -0500

    Ocultamos el título "Insomnia" cuando la resoulción sea sm

--- src/app/NavBar.js
+++ src/app/NavBar.js
@@ -7,8 +7,8 @@ export default function NavBar() {
       <nav className='sticky top-0 bg-noche-900/90 border-b border-noche-700/30 backdrop-blur text-stone-300 p-4 flex items-center justify-between'>
         <div className='relative flex items-center'>
           <Link href='/' className='relative flex items-center'>
-            <Image className='' src='https://media.discordapp.net/attachments/1032516925617614878/1107528348118294548/prelogo1.png?width=458&height=458' height={40} width={40} quality={100} alt='Logo de insomnia' />
-            <h1 className='text-2xl font-semibold pl-2 pr-4'>Insomnia</h1>
+            <Image className='mr-4' src='https://media.discordapp.net/attachments/1032516925617614878/1107528348118294548/prelogo1.png?width=458&height=458' height={40} width={40} quality={100} alt='Logo de insomnia' />
+            <h1 className='text-2xl font-semibold pr-4 md:block hidden'>Insomnia</h1>
           </Link>
           <ul className="relative font-semibold flex items-center gap-5 text-sm">
             <li><Link className='hover:text-white duration-200' href='/destacados'>Destacados</Link></li>
