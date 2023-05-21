57a23e4dfac40460b7c670f713dc306a62c22011
aAnfitrion <tendondevastador@hotmail.com>
2023-05-21 00:47:28 -0500

    Hacemos la vista responsive

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <div className='my-8 lg:mx-16 mx-4'>
       <div className='grid grid-cols-12'>
-        <div className="col-span-4">
+        <div className="md:col-span-4 col-span-12">
           <Tarjeta
             className='p-2'
             enlace='/blog/tecla-3'
