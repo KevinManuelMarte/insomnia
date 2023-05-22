7613c6c9338c0a0e2cf7c21b36d5ec806658a8a1
aAnfitrion <tendondevastador@hotmail.com>
2023-05-22 02:50:46 -0500

    Ajustando la vista para móviles

--- src/app/Tarjeta.js
+++ src/app/Tarjeta.js
@@ -4,23 +4,23 @@ import Image from 'next/image'
 
 export default function Tarjeta(props) {
   return (
-    <div className={props.className} key={props.key}>
-      <Link href={props.enlace} className="bg-noche-700/20 rounded-lg p-8 flex justify-between items-center flex-wrap">
+    <div className={`${props.className} bg-noche-700/20 rounded-lg`} key={props.key}>
+      <Link href={props.enlace} className="md:p-8 p-4 flex justify-between items-center flex-wrap">
         <div className='w-5/6'>
-          <div className='flex items-center text-white text-xl gap-2'>
+          <div className='flex items-center text-white md:text-xl text-lg gap-2'>
             {props.children}
             <p>{ props.titulo }</p>
             {props.distincion ? (
-              <Distincion className='text-sm' tipo={props.distincion} />
+              <Distincion className='md:text-sm text-xs' tipo={props.distincion} />
             ) : null}
           </div>
-          <p className='text-noche-300'>{ props.subtitulo }</p>
+          <p className='text-noche-300 md:text-base text-sm'>{ props.subtitulo }</p>
         </div>
         {props.img_src ? (
           <Image className='rounded-full w-1/6' src={props.img_src} height={70} width={70} quality={100} alt={props.img_alt} />
         ) : null}
         <div className='pt-2'>
-          <p className="text-noche-200">
+          <p className="text-noche-200 md:text-base text-sm">
             { props.desc }
           </p>
         </div>
