bc7572b95c52cea1329e367611f6c0459150cbec
aAnfitrion <tendondevastador@hotmail.com>
2023-05-22 03:32:16 -0500

    Ajustamos la vista para celular

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,19 +1,31 @@
 import Tarjeta from '../Tarjeta'
 import { HiOutlineCube } from 'react-icons/hi'
+import { BsShieldPlus } from 'react-icons/bs'
 
 export default function Page() {
   return (
-    <div className='my-8 lg:mx-16 mx-4'>
-      <div className='grid grid-cols-12'>
+    <div className='my-8 xl:mx-16 md:mx-8 mx-2'>
+      <div className='grid grid-cols-12 gap-x-4'>
         <div className="md:col-span-4 col-span-12">
           <Tarjeta
-            className='p-2'
+            className='mb-4'
             enlace='/blog/tecla-3'
             titulo='Tecla 3'
             subtitulo='Publicación activa'
             desc='Un servidor amistoso de Minecraft en modo supervivencia.'
           >
             <HiOutlineCube/>
+          </Tarjeta>
+        </div>
+        <div className="md:col-span-4 col-span-12">
+          <Tarjeta
+            className='mb-4'
+            enlace='/blog/se-busca-personal'
+            titulo='Se busca personal'
+            subtitulo='Publicación activa'
+            desc='Aplica para ser un miembro del personal.'
+          >
+            <BsShieldPlus />
           </Tarjeta>
         </div>
       </div>
--- src/app/blog/tecla-3/page.js
+++ src/app/blog/tecla-3/page.js
@@ -14,39 +14,35 @@ const encargados = perfiles.filter((perfil) => {
 export default function Page() {
   return (
     <>
-      <div className='my-8 lg:mx-16 mx-4'>
-        <div className='grid grid-cols-12'>
+      <div className='my-8 xl:mx-16 md:mx-8 mx-2'>
+        <div className='grid grid-cols-12 gap-4'>
           <div className="col-start-5 col-span-4">
-            <h1 className='text-yellow-300 md:text-4xl text-3xl font-semibold text-center md:leading-relaxed leading-relaxed'>Tecla 3</h1>
+            <h1 className='text-yellow-300 lg:text-4xl md:text-2xl text-xl font-semibold text-center lg:leading-relaxed md:leading-relaxed leading-relaxed'>Tecla 3</h1>
           </div>
           <div className="col-start-10 col-span-3">
-            <p className='text-neutral-300/90 text-right'>
+            <p className='text-noche-300 text-right lg:text-base text-sm'>
               17/5/2023
             </p>
           </div>
-          <div className="col-span-12 border-b border-yellow-300/90"></div>
-          <div className="col-span-12">
-            <p className='text-white leading-relaxed text-lg py-4'>
+          <div className="col-span-12 border-t border-yellow-300/90">
+            <p className='text-noche-100 leading-relaxed md:text-lg text-sm md:pt-4 pt-2'>
               Nos complace anunciar el regreso esta serie de servidores de Minecraft. Tecla regresa con su tercera edición, esta vez en la versión de Minecraft 1.20.
             </p>
           </div>
           <div className="col-span-12">
-            <h1 className='text-yellow-300 text-2xl font-semibold text-center leading-relaxed'>
+            <h1 className='text-yellow-300 lg:text-2xl md:text-2xl text-xl font-semibold text-center lg:leading-relaxed md:leading-relaxed leading-relaxed'>
               ¿De que tratará?
             </h1>
-          </div>
-          <div className="col-span-12">
-            <p className='text-white leading-relaxed text-lg pb-8'>
+            <p className='text-noche-100 leading-relaxed md:text-lg text-sm'>
               Este será un servidor amistoso en modo supervivencia semi vanilla, que organizará varios eventos. Es necesario estar en un equipo para participar en los eventos creados por los administradores, sin embargo, no para los eventos creados por los usuarios.
             </p>
           </div>
           <div className="md:col-span-6 col-span-12">
-            <div className="mb-4">
-              <h1 className='text-yellow-300 text-2xl font-semibold leading-relaxed flex items-center'>
-                <BsXCircle className='mr-2' />
+              <h1 className='text-yellow-300 lg:text-2xl md:text-2xl text-xl font-semibold text-center lg:leading-relaxed md:leading-relaxed leading-relaxed flex items-center gap-2'>
+                <BsXCircle/>
                 Lo que no se permite
               </h1>
-              <ul className='list-decimal list-inside text-white leading-relaxed text-lg pr-2'>
+              <ul className='list-decimal list-inside text-noche-100 leading-relaxed md:text-lg text-sm'>
                 <li>Usar cualquier clase de programa o cliente modificado (hacks) para obtener venta injustamente.</li>
                 <li>Matar a los jugadores, no importa el motivo.</li>
                 <li>Manipular las estructuras de otros jugadores.</li>
@@ -54,15 +50,13 @@ export default function Page() {
                 <li>Usar bugs de duplicación de inventario.</li>
                 <li>Ser tóxico con cualquier jugador.</li>
               </ul>
-            </div>
           </div>
           <div className="md:col-span-6 col-span-12">
-            <div className="mb-4">
-              <h1 className='text-yellow-300 text-2xl font-semibold leading-relaxed flex items-center'>
-                <BsCheckCircle className='mr-2' />
+              <h1 className='text-yellow-300 lg:text-2xl md:text-2xl text-xl font-semibold text-center lg:leading-relaxed md:leading-relaxed leading-relaxed flex items-center gap-2'>
+                <BsCheckCircle />
                 Lo que se permite
               </h1>
-              <ul className='list-decimal list-inside text-white leading-relaxed text-lg'>
+              <ul className='list-decimal list-inside text-noche-100 leading-relaxed md:text-lg text-sm'>
                 <li>Usar mods de: minimapa, interfaz de información, interfaz de objetos, optimización, decoración y arreglo de bugs.</li>
                 <li>Formar alianzas entre equipo.</li>
                 <li>Acordar duelos a muerte con otros jugadores/equipos.</li>
@@ -70,62 +64,51 @@ export default function Page() {
                 <li>No participar en los eventos.</li>
                 <li>Usar bugs de duplicación de tnt, arena, grava y alfombras.</li>
               </ul>
+          </div>
+          <div className="col-span-12">
+            <h1 className='text-yellow-300 lg:text-2xl md:text-2xl text-xl font-semibold text-center lg:leading-relaxed md:leading-relaxed leading-relaxed pb-2'>Características del servidor</h1>
+            <div className="flex justify-left flex-wrap gap-x-4">
+              <div className="flex items-center md:text-lg text-sm">
+                <BsMemory className='text-ocaso-300' />
+                <p className='text-ocaso-300 font-semibold mx-2'>Memoria del servidor:</p>
+                <p className='text-noche-100'>2GB</p>
+              </div>
+              <div className="flex items-center md:text-lg text-sm">
+                <BsPerson className='text-ocaso-300' />
+                <p className='text-ocaso-300 font-semibold mx-2'>Máximos jugadores en línea:</p>
+                <p className='text-noche-100'>10</p>
+              </div>
+              <div className="flex items-center md:text-lg text-sm">
+                <RxCube className='text-ocaso-300' />
+                <p className='text-ocaso-300 font-semibold mx-2'>Versión del juego:</p>
+                <p className='text-noche-100'>1.20</p>
+              </div>
+              <div className="flex items-center md:text-lg text-sm">
+                <TbBucket className='text-ocaso-300' />
+                <p className='text-ocaso-300 font-semibold mx-2'>Jugabilidad:</p>
+                <p className='text-noche-100'>Semi-vanilla</p>
+              </div>
+              <div className="flex items-center md:text-lg text-sm">
+                <BsCpu className='text-ocaso-300' />
+                <p className='text-ocaso-300 font-semibold mx-2'>Cpu:</p>
+                <p className='text-noche-100'>2 Núcleos</p>
+              </div>
+              <div className="flex items-center md:text-lg text-sm">
+                <BiWorld className='text-ocaso-300' />
+                <p className='text-ocaso-300 font-semibold mx-2'>Ubicación del servidor:</p>
+                <p className='text-noche-100'>Canadá</p>
+              </div>
             </div>
           </div>
           <div className="col-span-12">
-            <h1 className='text-yellow-300 text-2xl font-semibold leading-relaxed text-center'>Características del servidor</h1>
-          </div>
-          <div className="md:col-span-4 col-span-12">
-            <div className="flex items-center text-lg">
-              <BsMemory className='text-yellow-300' />
-              <p className='text-yellow-300 font-semibold mx-2'>Memoria del servidor:</p>
-              <p className='text-white'>2GB</p>
-            </div>
-          </div>
-          <div className="md:col-span-4 col-span-12">
-            <div className="flex items-center text-lg">
-              <BsPerson className='text-yellow-300' />
-              <p className='text-yellow-300 font-semibold mx-2'>Máximos jugadores en línea:</p>
-              <p className='text-white'>10</p>
-            </div>
-          </div>
-          <div className="md:col-span-4 col-span-12">
-            <div className="flex items-center text-lg">
-              <RxCube className='text-yellow-300' />
-              <p className='text-yellow-300 font-semibold mx-2'>Versión del juego:</p>
-              <p className='text-white'>1.20</p>
-            </div>
-          </div>
-          <div className="md:col-span-4 col-span-12">
-            <div className="flex items-center text-lg">
-              <TbBucket className='text-yellow-300' />
-              <p className='text-yellow-300 font-semibold mx-2'>Jugabilidad:</p>
-              <p className='text-white'>Semi-vanilla</p>
-            </div>
-          </div>
-          <div className="md:col-span-4 col-span-12">
-            <div className="flex items-center text-lg">
-              <BsCpu className='text-yellow-300' />
-              <p className='text-yellow-300 font-semibold mx-2'>Cpu:</p>
-              <p className='text-white'>2 núcleos</p>
-            </div>
-          </div>
-          <div className="md:col-span-4 col-span-12">
-            <div className="flex items-center text-lg">
-              <BiWorld className='text-yellow-300' />
-              <p className='text-yellow-300 font-semibold mx-2'>Ubicación del servidor:</p>
-              <p className='text-white'>Canadá</p>
-            </div>
+            <h1 className="text-yellow-300 lg:text-2xl md:text-2xl text-xl font-semibold text-center lg:leading-relaxed md:leading-relaxed leading-relaxed">Personal a cargo de este servidor</h1>
           </div>
           <div className="col-span-12">
-            <h1 className="text-2xl text-yellow-300 font-semibold text-center py-4">Personal a cargo de este servidor</h1>
-          </div>
-          {encargados.map(perfil => {
-            return (
-              <div className="md:col-span-6 col-span-12" key={perfil.usuario}>
-                <ul className="list-none">
+            <div className="grid grid-cols-2 gap-4">
+              {encargados.map(perfil => {
+                return (
                   <Tarjeta
-                    className='p-2'
+                    className='col-span-2 md:col-span-1'
                     enlace={`/destacados/${perfil.usuario}`}
                     key={perfil.usuario}
                     titulo={perfil.usuario}
@@ -134,14 +117,12 @@ export default function Page() {
                     img_src={perfil.img_src}
                     img_alt={`Imagen de perfil de ${perfil.usuario}`}
                   />
-                </ul>
-              </div>
-            )
-          })}
-          <div className="md:col-start-4 md:col-span-6 col-span-12">
-            <div className="py-8">
-              <Invitacion />
+                )
+              })}
             </div>
+          </div>
+          <div className="md:col-start-4 md:col-span-6 col-span-12">
+            <Invitacion />
           </div>
         </div>
       </div>
