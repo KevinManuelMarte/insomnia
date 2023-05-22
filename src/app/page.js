1b246f111024ef71a538909b80e62b702d7c0e7f
aAnfitrion <tendondevastador@hotmail.com>
2023-05-22 15:35:30 -0500

    porba2

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
           <Image src='https://media.discordapp.net/attachments/1032516925617614878/1107528348118294548/prelogo1.png?width=1024&height=1024' width={1024} height={1024} alt='Logo de Insomnia' quality={100} />
         </div>
         <div className='2xl:col-start-4 xl:col-start-2 2xl:col-start-2 col-start-2 xl:col-span-3 2xl:col-span-2 col-span-3 text-stone-100 text-center'>
-          <h1 className='xl:text-5xl 2xl:text-7xl text-4xl xl:leading-relaxed leading-relaxed 2xl:leading-tight'>Insomnia</h1>
+          <h1 className='xl:text-5xl 2xl:text-7xl text-4xl xl:leading-relaxed leading-relaxed 2xl:leading-tight'>a</h1>
           <p className='xl:text-xl 2xl:text-3xl'>Esta es la página oficial del servidor de Discord Insomnia.</p>
         </div>
       </div>
