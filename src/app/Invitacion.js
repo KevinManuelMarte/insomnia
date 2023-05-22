56ff1761f55e426f29628e21772796a76e5c091b
aAnfitrion <tendondevastador@hotmail.com>
2023-05-22 10:21:51 -0500

    Hacemos que el subtitulo sea dinámico

--- src/app/Invitacion.js
+++ src/app/Invitacion.js
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 import { BsPerson } from 'react-icons/bs'
 
-export default function Invitacion() {
+export default function Invitacion(props) {
   return (
 
         <div className='bg-ocaso-800/50 rounded-lg px-4 pt-1 pb-2'>
           <div className='grid grid-cols-8'>
             <div className='col-span-8'>
-              <p className='text-sm font-bold text-neutral-300/90 py-2'>¡Únete a Insomnia para disfrutar de este evento!</p>
+          <p className='text-sm font-bold text-neutral-300/90 py-2'>{props.subtitulo}</p>
             </div>
             <div className='col-span-1'>
               <Image src='https://media.discordapp.net/attachments/1032516925617614878/1107528348118294548/prelogo1.png?width=458&height=458' height={256} width={256} alt='Logo de Insomnia' quality={100} />
