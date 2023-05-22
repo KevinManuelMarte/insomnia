2ab00d77f8efc34d91c930b1b52384d8efc462a8
aAnfitrion <tendondevastador@hotmail.com>
2023-05-22 11:30:56 -0500

    Agregamos varios mini componentes

---
+++ src/app/Mini.js
@@ -0,0 +1,60 @@
+export function Contenedor(props) {
+  return (
+    <div className="my-8 xl:mx-16 md:mx-8 mx-2">
+      {props.children}
+    </div>
+  )
+}
+
+export function Titulo1(props) {
+  return (
+    <h1 className={`
+    ${props.className}
+    lg:text-4xl md:text-2xl text-xl font-semibold`}>
+      {props.children}
+    </h1>
+  )
+}
+
+export function Titulo2(props) {
+  return (
+    <h2 className={`
+    ${props.className}
+    lg:text-3xl md:text-xl text-lg font-semibold`}>
+      {props.children}
+    </h2>
+  )
+}
+
+export function Subtitulo(props) {
+  return (
+    <h3 className={`
+    ${props.className}
+    lg:text-xl md:text-lg text-sm
+    `}>
+      {props.children}
+    </h3>
+  )
+}
+
+export function Parrafo(props) {
+  return (
+    <p className={`
+    ${props.className}
+    lg:text-xl md:text-lg text-sm
+    `}>
+      {props.children}
+    </p>
+  )
+}
+
+export function Lista(props) {
+  return (
+    <ul className={`
+    ${props.className}
+    md:text-lg text-sm
+    `}>
+      {props.children}
+    </ul>
+  )
+}
