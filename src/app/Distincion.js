9ad17ff849a44fca1070f5ab994eb5a867e3e8da
aAnfitrion <tendondevastador@hotmail.com>
2023-05-23 01:34:12 -0500

    Agregamos la distincion de moderador interno

--- src/app/Distincion.js
+++ src/app/Distincion.js
@@ -1,76 +1,52 @@
 export default function Distincion(props) {
-  if (!props.op) {
-    return (
-      <span
-        key={props.key}
-        className={`
+  return (
+    <span
+      key={props.key}
+      className={`
         border rounded-full
         ${props.className}
         ${props.tipo == 1 ? 'text-emerald-400 border-emerald-400' : ''}
         ${props.tipo == 2 ? 'text-red-400 border-red-400' : ''}
         ${props.tipo == 3 ? 'text-lime-400 border-lime-400' : ''}
-        ${props.tipo == 4 ? 'text-sky-300 border-sky-300' : ''}
-        ${props.tipo == 5 ? 'text-fuchsia-300 border-fuchsia-300' : ''}
-        ${props.tipo == 6 ? 'text-amber-400 border-amber-400' : ''}
-        ${props.tipo == 7 ? 'text-orange-400 border-orange-400' : ''}
-        ${props.tipo == 8 ? 'text-pink-400 border-pink-400' : ''}
-        ${props.tipo == 9 ? 'text-rose-300 border-rose-300' : ''}
-        ${props.tipo == 10 ? 'text-violet-400 border-violet-400' : ''}
-        ${props.tipo == 11 ? 'text-orange-200 border-orange-200' : ''}
+        ${props.tipo == 4 ? 'text-teal-300 border-teal-300' : ''}
+        ${props.tipo == 5 ? 'text-sky-300 border-sky-300' : ''}
+        ${props.tipo == 6 ? 'text-fuchsia-300 border-fuchsia-300' : ''}
+        ${props.tipo == 7 ? 'text-amber-400 border-amber-400' : ''}
+        ${props.tipo == 8 ? 'text-orange-400 border-orange-400' : ''}
+        ${props.tipo == 9 ? 'text-pink-400 border-pink-400' : ''}
+        ${props.tipo == 10 ? 'text-rose-300 border-rose-300' : ''}
+        ${props.tipo == 11 ? 'text-violet-400 border-violet-400' : ''}
+        ${props.tipo == 12 ? 'text-orange-200 border-orange-200' : ''}
         
-        ${props.tipo == 'c' ? 'text-white border-white' : null}
-        ${props.tipo == 'r' ? 'text-white border-white' : null}
-        ${props.tipo == 'u' ? 'text-white border-white' : null}
-        ${props.tipo == 'e' ? 'text-white border-white' : null}
-        ${props.tipo == 'l' ? 'text-white border-white' : null}
+        ${props.tipo == 'c' ? 'text-white border-white' : ''}
+        ${props.tipo == 'r' ? 'text-white border-white' : ''}
+        ${props.tipo == 'u' ? 'text-white border-white' : ''}
+        ${props.tipo == 'e' ? 'text-white border-white' : ''}
+        ${props.tipo == 'l' ? 'text-white border-white' : ''}
 
         ${props.tipo == 'T2' ? 'text-green-400 border-green-400' : ''}
         `}
-      >
-        {props.tipo == 1 ? 'Administrador' : null}
-        {props.tipo == 2 ? 'Mano derecha' : null}
-        {props.tipo == 3 ? 'Jefe moderador' : null}
-        {props.tipo == 4 ? 'Moderador' : null}
-        {props.tipo == 5 ? 'Donador' : null}
-        {props.tipo == 6 ? 'Programador' : null}
-        {props.tipo == 7 ? 'Publicista' : null}
-        {props.tipo == 8 ? 'Anfitrión' : null}
-        {props.tipo == 9 ? 'Diseñador' : null}
-        {props.tipo == 10 ? 'Soporte' : null}
-        {props.tipo == 11 ? 'Pride guardián' : null}
+    >
+      {props.tipo == 1 ? 'Administrador' : null}
+      {props.tipo == 2 ? 'Mano derecha' : null}
+      {props.tipo == 3 ? 'Jefe moderador' : null}
+      {props.tipo == 4 ? 'Moderador' : null}
+      {props.tipo == 5 ? 'Moderador interno' : null}
+      {props.tipo == 6 ? 'Donador' : null}
+      {props.tipo == 7 ? 'Programador' : null}
+      {props.tipo == 8 ? 'Publicista' : null}
+      {props.tipo == 9 ? 'Anfitrión' : null}
+      {props.tipo == 10 ? 'Diseñador' : null}
+      {props.tipo == 11 ? 'Soporte' : null}
+      {props.tipo == 12 ? 'Pride guardián' : null}
 
-        {props.tipo == 'c' ? 'Común' : null}
-        {props.tipo == 'r' ? 'Raro' : null}
-        {props.tipo == 'u' ? 'Único' : null}
-        {props.tipo == 'e' ? 'Épico' : null}
-        {props.tipo == 'l' ? 'Legendario' : null}
+      {props.tipo == 'c' ? 'Común' : null}
+      {props.tipo == 'r' ? 'Raro' : null}
+      {props.tipo == 'u' ? 'Único' : null}
+      {props.tipo == 'e' ? 'Épico' : null}
+      {props.tipo == 'l' ? 'Legendario' : null}
 
-        {props.tipo == 'T2' ? 'Estelar Tecla 2' : null}
-      </span>
-    )
-  } else {
-    return (
-      <span key={props.key} className={`border rounded-full py-1 px-1 leading-none text-white border-white ${props.className}`}>
-        {props.tipo == 1 ? 'Administrador' : null}
-        {props.tipo == 2 ? 'Mano derecha' : null}
-        {props.tipo == 3 ? 'Jefe moderador' : null}
-        {props.tipo == 4 ? 'Moderador' : null}
-        {props.tipo == 5 ? 'Donador' : null}
-        {props.tipo == 6 ? 'Programador' : null}
-        {props.tipo == 7 ? 'Publicista' : null}
-        {props.tipo == 8 ? 'Anfitrión' : null}
-        {props.tipo == 9 ? 'Diseñador' : null}
-        {props.tipo == 10 ? 'Soporte' : null}
-        {props.tipo == 11 ? 'Pride guardián' : null}
-
-        {props.tipo == 'c' ? 'Común' : null}
-        {props.tipo == 'r' ? 'Raro' : null}
-        {props.tipo == 'u' ? 'Único' : null}
-        {props.tipo == 'e' ? 'Épico' : null}
-        {props.tipo == 'l' ? 'Legendario' : null}
-
-        {props.tipo == 'T2' ? 'Estelar Tecla 2' : null}
-      </span>
-    )
-  }
+      {props.tipo == 'T2' ? 'Estelar Tecla 2' : null}
+    </span>
+  )
 }
