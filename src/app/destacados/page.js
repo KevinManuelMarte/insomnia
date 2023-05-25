2f484b64ae8c153d42c772264509ac9bedd31986
aAnfitrion <tendondevastador@hotmail.com>
2023-05-25 10:26:45 -0500

    Agregamos responsividad

--- src/app/destacados/page.js
+++ src/app/destacados/page.js
@@ -3,11 +3,12 @@ import Tarjeta from '../Tarjeta'
 
 export default function Page() {
 	return (
-		<div className="grid grid-cols-3 gap-4">
+		<div className="grid grid-cols-12 gap-4">
 			{perfiles.map(perfil => {
 				return (
 					<Tarjeta
 						key={perfil.usuario_simple}
+						className='lg:col-span-4 md:col-span-6 col-span-12'
 						enlace={`/destacados/${perfil.usuario_simple}`}
 						titulo={perfil.usuario}
 						distincion={perfil.distincion}
