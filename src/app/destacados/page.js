2488caea832107c53f71da5c2522eda7afe35cbb
aAnfitrion <tendondevastador@hotmail.com>
2023-05-26 03:23:23 -0500

    no nos rendimos y le damos un bonito estilo a las tarjetas

--- src/app/destacados/page.js
+++ src/app/destacados/page.js
@@ -1,22 +1,25 @@
-import { perfiles } from '../Perfiles'
-import Tarjeta from '../Tarjeta'
+import { perfiles } from "../Perfiles";
+import Tarjeta from "../Tarjeta";
 
 export default function Page() {
-	return (
-		<div className="grid grid-cols-12 gap-4">
-			{perfiles.map(perfil => {
-				return (
-					<Tarjeta
-						key={perfil.usuario_simple}
-						className='lg:col-span-4 md:col-span-6 col-span-12'
-						enlace={`/destacados/${perfil.usuario_simple}`}
-						titulo={perfil.usuario}
-						distincion={perfil.distincion}
-						img_src={perfil.img_src}
-						subtitulo={perfil.cargo}
-					/>
-				)
-			})}
-		</div>
-	)
+  return (
+    <div className="grid grid-cols-3  gap-4">
+      {perfiles.map((perfil, idx) => {
+        return (
+          <Tarjeta
+            key={perfil.usuario_simple}
+            className={
+              (idx === 0) || (idx === 2) ? `row-span-1` : `row-span-2`
+            }
+            enlace={`/destacados/${perfil.usuario_simple}`}
+            titulo={perfil.usuario}
+            distincion={perfil.distincion}
+            img_src={perfil.img_src}
+            subtitulo={perfil.cargo}
+            desc={perfil.desc}
+          />
+        );
+      })}
+    </div>
+  );
 }
