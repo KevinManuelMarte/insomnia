4902713b1555877736b9ba2da77845c32fc3ac43
aAnfitrion <tendondevastador@hotmail.com>
2023-05-29 04:23:36 -0500

    Creamos este componente

---
+++ components/Container.tsx
@@ -0,0 +1,11 @@
+interface Prope {
+  children: JSX.Element
+}
+
+export default function Container(props: Prope): JSX.Element {
+  return (
+    <div className="2xl:px-32 lg:px-16 md:px-4 px-2">
+      {props.children}
+    </div>
+  )
+}
