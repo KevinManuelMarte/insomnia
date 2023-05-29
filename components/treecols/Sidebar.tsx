2531609d3e96d77f402f48f9d116afa4516dfbf2
aAnfitrion <tendondevastador@hotmail.com>
2023-05-29 04:06:36 -0500

    Quítamos las columnas preestablecidas

--- components/treecols/Sidebar.tsx
+++ components/treecols/Sidebar.tsx
@@ -5,7 +5,7 @@ interface Prope {
 export default function Sidebar(props:Prope): JSX.Element {
   return (
     <div className="2xl:col-span-2 xl:col-span-3 lg:col-span-3 lg:block hidden">
-      <div className="grid grid-cols-12 sticky top-24">
+      <div className="grid grid-cols-12 sticky top-24 gap-4">
         {props.children}
       </div>
     </div>
