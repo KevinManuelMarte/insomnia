b4fe2bfede595005db39aa9460387813286edbad
aAnfitrion <tendondevastador@hotmail.com>
2023-05-29 04:05:58 -0500

    Le cambiamos el nombre a este componente

--- components/treecols/Midcol.tsx
+++ components/treecols/Midcol.tsx
@@ -2,7 +2,7 @@ interface Prope {
   children: JSX.Element;
 }
 
-export default function Container(props: Prope): JSX.Element {
+export default function Midcol(props: Prope): JSX.Element {
   return (
     <div className="2xl:col-span-8 xl:col-span-6 lg:col-span-6 col-span-12 px-4">
       <div className="grid grid-cols-12 gap-4">
