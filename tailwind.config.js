391c458dcb8aa4ab52e119cc77dac62a4e5ad08c
aAnfitrion <tendondevastador@hotmail.com>
2023-05-31 02:05:49 -0500

    Añadimos la paleta de colores ex

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}'
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
+      transparent: "transparent",
+      current: "currentColor",
       black: colors.black,
       white: colors.white,
       slate: colors.slate,
@@ -34,29 +34,32 @@ module.exports = {
       fuchsia: colors.fuchsia,
       pink: colors.pink,
       rose: colors.rose,
-      'noche': {
-        100: '#e2dbf0',
-        200: '#c5b7e1',
-        300: '#a893d2',
-        400: '#8b6fc3',
-        500: '#6e4bb4',
-        600: '#583c90',
-        700: '#422d6c',
-        800: '#2c1e48',
-        900: '#160f24'
+      "noche": {
+        100: "#e2dbf0",
+        200: "#c5b7e1",
+        300: "#a893d2",
+        400: "#8b6fc3",
+        500: "#6e4bb4",
+        600: "#583c90",
+        700: "#422d6c",
+        800: "#2c1e48",
+        900: "#160f24",
       },
-      'ocaso': {
-        900: '#240f23',
-        800: '#481e46',
-        700: '#6c2d69',
-        600: '#903c8c',
-        500: '#b44baf',
-        400: '#c36fbf',
-        300: '#d293cf',
-        200: '#e1b7df',
-        100: '#f0dbef'
-      }
+      "ocaso": {
+        900: "#240f23",
+        800: "#481e46",
+        700: "#6c2d69",
+        600: "#903c8c",
+        500: "#b44baf",
+        400: "#c36fbf",
+        300: "#d293cf",
+        200: "#e1b7df",
+        100: "#f0dbef",
+      },
+      "ex": {
+        100: "#221532",
+      },
     },
   },
   plugins: [],
-}
+};
